584f683f4518a1def10c004d4faa56b9dbad98d5
Nahnahkessie <nahnahkessie@gmail.com>
2022-10-21 14:27:09 -0400

    codes

---
+++ numbs.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * convert - converter function, a clone of itoa
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ * @params: parameter struct
+ *
+ * Return: string
+ */
+char *convert(long int num, int base, int flags, params_t *params)
+{
+        static char *array;
+        static char buffer[50];
+        char sign = 0;
+        char *ptr;
+        unsigned long n = num;
+        (void)params;
+  
+        if (!(flags & CONVERT_UNSIGNED) && num < 0)
+        {
+                n = -num;
+                sign = '-';
+        }
+        array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "
+        ptr = &buffer[49];
+        *ptr = '\0';
+  
+        do      {
+                *--ptr = array[n % base];
+                n /= base;
+        } while (n != 0);
+  
+        if (sign)
+                *--ptr = sign;
+        return (ptr);
+}
+
+/**
+ * print_unsigned - prints unsigned integer numbers
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: bytes printed
+ */
+int printed_unsigned(va_list app, params_t *params)
+{
+        unsigned long 1;
+  
+        if (params->1_modifier)
+                1 = (unsigned long)va_arg(ap, unsigned long);
+        else if (params->h_modifier)
+                1 = (unsigned short int)va_arg(ap, unsigned int);
+        else
+                1 = (unsigned int)va_arg(ap, unsigned int);
+        params->unsign = 1;
+        return (print_number(convert(1, 10, CONVERT_UNSIGNED, params)
+                       
